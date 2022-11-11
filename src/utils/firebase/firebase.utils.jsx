7ce31b6d3136a9c31253d14a3438620c3d4416ed
haisoftwarevn <haisoftwarevn@gmail.com>
2022-11-11 10:24:25 +0700

    login popup and redirect with firebase and restore user in firestore

---
+++ src/utils/firebase/firebase.utils.jsx
@@ -0,0 +1,54 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBBlksgUBnTNgr3rxA51s1-Kf2KJPP0cgU",
+  authDomain: "crwn-clothing-db-dff28.firebaseapp.com",
+  projectId: "crwn-clothing-db-dff28",
+  storageBucket: "crwn-clothing-db-dff28.appspot.com",
+  messagingSenderId: "521493881974",
+  appId: "1:521493881974:web:864c42dfb8eacd332f2b57",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedicrect = () =>
+  signInWithRedirect(auth, googleProvider);
+export const db = getFirestore();
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+  if (!userSnapshot.exists()) {
+    //todo
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await setDoc(userDocRef, { displayName, email, createdAt });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userDocRef;
+};
