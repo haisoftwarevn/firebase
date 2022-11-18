eafd2ede5b65f653b8a685778101b95ba30bb9df
haisoftwarevn <haisoftwarevn@gmail.com>
2022-11-18 08:09:49 +0700

    full redux-persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,32 @@
 import { compose, createStore, applyMiddleware } from "redux";
 
 import { persistStore, persistReducer } from "redux-persist";
-
+import storage from "redux-persist/lib/storage";
 //import { legacy_createStore as createStore } from 'redux';
 // phòng trường hợp chạy không được
 import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [logger];
+///middle ware
+const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
+  Boolean
+);
 
 const composeEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composeEnhancers);
+/////////////////////
+
+/////////////persit
+const configPersist = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(configPersist, rootReducer);
+
+/////////////////store
+export const store = createStore(persistedReducer, undefined, composeEnhancers);
+
+export const persistor = persistStore(store);
